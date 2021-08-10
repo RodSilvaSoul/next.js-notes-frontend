5eb3bb05a1e964d9caaa5a8928c55333ac43c08b
rodrigo silva <rodsilvasoul@gmail.com>
2021-08-10 11:31:34 -0400

    chore: eslint added new rules

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,23 +1,25 @@
 import { createGlobalStyle } from 'styled-components';
 
-export const GlobalStyles = createGlobalStyle`
-	* {
-		box-sizing: border-box;
+const GlobalStyles = createGlobalStyle`
+  * {
+	  box-sizing: border-box;
 		padding: 0;
 		margin: 0;
 	}
 	
 	a {
 		text-decoration: none;
-	}
+  }
 
-	html {
+  html {
 		@media(max-width: ${(props) => props.theme.breakPoints.lg}) {
 			font-size: 93.75%;
-		}
+	  }
 
 		@media(max-width: ${(props) => props.theme.breakPoints.md}) {
 			font-size: 87.5%; ;
 		}
 	}
 `;
+
+export { GlobalStyles };
