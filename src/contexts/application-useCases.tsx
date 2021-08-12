248504674395de8c59337766a078e8858faa2564
rodrigo silva <rodsilvasoul@gmail.com>
2021-08-12 15:36:44 -0400

    feat: aplication use case provider

---
+++ src/contexts/application-useCases.tsx
@@ -0,0 +1,47 @@
+import {
+  useContext,
+  createContext,
+  ReactNode,
+  useCallback,
+  useState,
+} from 'react';
+
+interface ApplicationUseCase {
+  addNewNote: () => void;
+  cancelNewNote: () => void;
+  isNoteTextAreaVisible: boolean;
+}
+
+const ApplicationUseCaseContext = createContext<ApplicationUseCase>({} as any);
+
+interface ApplicationUseCaseProviderProps {
+  children: ReactNode;
+}
+
+export const ApplicationUseCaseProvider = ({
+  children,
+}: ApplicationUseCaseProviderProps) => {
+  const [isNoteTextAreaVisible, setIsNoteTextAreaVisible] = useState(false);
+
+  const addNewNote = useCallback(() => {
+    setIsNoteTextAreaVisible(true);
+  }, []);
+
+  const cancelNewNote = useCallback(() => {
+    setIsNoteTextAreaVisible(false);
+  }, []);
+
+  return (
+    <ApplicationUseCaseContext.Provider
+      value={{
+        addNewNote,
+        cancelNewNote,
+        isNoteTextAreaVisible,
+      }}
+    >
+      {children}
+    </ApplicationUseCaseContext.Provider>
+  );
+};
+
+export const useApplicationUseCase = () => useContext(ApplicationUseCaseContext);
